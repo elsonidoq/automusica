697a9103b6e525d4825f1776b1211da7226aa9a9
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2008-06-15 18:36:36 +0000

    termine el refactoring

--- src/tesis/training/__init__.py
+++ src/tesis/training/__init__.py
@@ -1,1 +1,8 @@
-import freedots
+from tesis.freedots import musicxml
+from tesis.hmm import hidden_markov_learner
+
+def train_hmm(fnames, score2obs_seq):
+	for fname in fnames:
+		score= musicxml.load_score(fname)
+		obs_seq= score2obs_seq(score)
+
