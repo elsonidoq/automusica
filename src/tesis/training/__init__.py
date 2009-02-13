07199e089cff200b88a695cdb69a70d5d03362bd
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-02-13 13:07:40 +0000

    mezclando con midistuff

--- src/tesis/training/__init__.py
+++ src/tesis/training/__init__.py
@@ -4,20 +4,17 @@ from tesis.hmm.hidden_markov_model import *
 from tesis.hmm.random_variable import ConstantRandomVariable
 from itertools import imap
 
-def train_hmm(fnames, score2obs_seq):
+def train_hmm(fnames, file2obs_seq):
     """
-    dada una lista de archivos y una funcion de traduccion que tome una Score y
+    dada una lista de archivos y una funcion de traduccion que tome un archivo y
     devuelva una observation sequence entrena un hmm y lo devuelve como
     resultado
     """
     learner= HiddenMarkovLearner()
     hidden_states= set()
     for i, fname in enumerate(fnames):
-        print "loading", fname
-        score= musicxml.load_file(fname)
-
-        print "\tconverting to observation seq"
-        obs_seq= score2obs_seq(score)
+        print "\tconverting `%s` to observation seq" % fname
+        obs_seq= file2obs_seq(fname)
 
         hidden_states.update(imap(lambda x:x[0], obs_seq))
 
@@ -28,9 +25,30 @@ def train_hmm(fnames, score2obs_seq):
     print "building result"
     return learner.get_trainned_model(initial_probability)
 
+from midistuff.writers.to_score import Score, Instrument
+from midistuff.midi_messages import MidiMessage
+def create_song(output_file, hmm, song_size, divisions):
+    """
+    dado un nombre de archivo, un hmm y un numero que determine el largo de la
+    observation seq, genera un midi y lo escribe en output_file
+    """
+    obs= list(SizedRandomObservation(hmm, song_size))
+    score= Score(divisions)
+    instrument= Instrument()
+    instrument.patch= 1
+    acum_time= 5
+    score.messages= [MidiMessage((96, 0, 3, 0, 0), 'smtp_offset', 0), MidiMessage((4, 2, 24, 8), 'time_signature', 0), MidiMessage((1, 0), 'key_signature', 0), MidiMessage((521739,), 'tempo', 0)]
+    for i, o in enumerate(obs):
+        pitch= o.values()[0]
+        duration= o.keys()[0].get_value()
+        score.note_played(instrument, pitch, acum_time, duration, 200)
+        acum_time+= duration
+
+    score.to_midi(output_file)
+    return []
 
 from tesis.freedots.playback import MidiWriter
-def create_song(output_file, hmm, song_size, divisions):
+def create_song_old(output_file, hmm, song_size, divisions):
     """
     dado un nombre de archivo, un hmm y un numero que determine el largo de la
     observation seq, genera un midi y lo escribe en output_file
@@ -61,13 +79,14 @@ class NoteWrapper(object):
 
 
 
-def simple_score2oseq(score):
+def simple_score2oseq(fname):
     """
     transforma una partitura en una secuencia de notas. 
     Los hidden states no representan nada mas que el tic
     en el que ocurre
     nombres que dependen 
     """
+    score= musicxml.load_file(fname)
     tic_number= 1
     res= []
     for part in score.parts:
@@ -84,11 +103,38 @@ def simple_score2oseq(score):
 
     return res
 
-def hidden_chord_s2observation(score):
+from midistuff.writers.to_score import MidiToScore
+from midistuff.readers.MidiInFile import MidiInFile
+class MidiObsSeq(object):
+    def __init__(self, patch):
+        """
+        patch es el instrumento que me va a interesart seguir
+        """
+        self.patch= patch
+
+    def __call__(self, fname):
+        hdlr= MidiToScore()
+        midi_in = MidiInFile(hdlr, fname)
+        midi_in.read()
+        for instrument, notes in hdlr.score.notes_per_instrument.iteritems():
+            if instrument.patch == self.patch:
+                return self._build_obs_seq(notes)
+    
+    def _build_obs_seq(self, notes):
+        res= []
+        for note in notes:
+            duration_var= ConstantRandomVariable(note.duration, 'duration')
+            res.append((note.note, {duration_var:note.duration}))
+
+        return res
+        
+    
+def hidden_chord_s2observation(fname):
     """
     estados ocultos: acordes
     observaciones: notes
     """
+    score= musicxml.load_file(fname)
     res= []
     for p in score.parts:
         for m in p.measures:
