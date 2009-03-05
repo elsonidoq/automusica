b1e0007dce467e6be759fe982f7f3e85b54d3933
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-03-05 12:53:31 +0000

    quantiza como debe ser

--- src/view_score_midi2ly.sh
+++ src/view_score_midi2ly.sh
@@ -2,7 +2,7 @@ cp $1 tmp.mid
 midi2ly tmp.mid
 convert-ly tmp-midi.ly > tmp-midi.c.ly
 lilypond tmp-midi.c.ly
-rm tmp.mid tmp-midi.ly tmp-midi.c.ly
+#rm tmp.mid tmp-midi.ly tmp-midi.c.ly
 mv tmp-midi.c.pdf $1.pdf
 rm tmp-midi.c.ps
 echo "output in $1.pdf"
