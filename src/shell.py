7fb8b8fbd12b3ecc9fc11204d31c3f20c3f9e48b
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-03-28 14:30:27 +0000

    organizando

--- src/shell.py
+++ src/shell.py
@@ -1,7 +1,7 @@
 from sys import argv
 from electrozart.parsing.midi import MidiScoreParser, MidiPatchParser, MidiScoreParserCache 
 from electrozart.algorithms.hmm import HmmAlgorithm, StructuredHmmAlgorithm
-from electrozart.algorithms.lsa import apply_lsa
+#from electrozart.algorithms.lsa import apply_lsa
 from electrozart.writing.midi import MidiScoreWriter
 
 fnames= ['beatles-0-10.mid',
