c99b2155b91aa43839222384366a7d4bc8963278
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-05-18 00:11:26 +0000

    convex

---
+++ src/electrozart/electrozart/algorithms/hmm/silence.py
@@ -0,0 +1,12 @@
+import random
+class SilenceAlg(object):
+    def __init__(self, interval_size):
+        self.interval_size= interval_size
+    def start_creation(self, context_score): pass
+    def next(self, result):
+        interval_size= self.interval_size
+        if result.start % interval_size >= 3.0/4*interval_size and \
+            random.randint(0,1) == 1:
+            result.pitch=-1
+
+    def train(self, score): pass
