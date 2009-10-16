87281cca8c76235c1996facebb678b04bd0e0139
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-10-16 23:54:36 +0000

    context algorithm factorizado

--- src/trunk/electrozart/electrozart/algorithms/hmm/melody/narmour_hmm.py
+++ src/trunk/electrozart/electrozart/algorithms/hmm/melody/narmour_hmm.py
@@ -127,27 +127,9 @@ class NarmourHMM(HmmAlgorithm):
     def __init__(self, *args, **kwargs):
         super(NarmourHMM, self).__init__(*args, **kwargs)
         self.obsSeqBuilder= IntervalsObsSeq(self.obsSeqBuilder)
-        self.matching_notes= defaultdict(lambda: defaultdict(lambda :0))
     
     def train(self, score):
         super(NarmourHMM, self).train(score)
-
-        notes= score.get_notes(skip_silences=True)
-        notes.sort(key=lambda n:n.start)
-
-        for i, n1 in enumerate(notes):
-            n1_can= n1.get_canonical_note()
-            # recorro todas las notas que suenan con n1
-            for j in xrange(i, len(notes)):
-                n2= notes[j]
-                # quiere decir que n2 no esta sonando con n1
-                if n2.start > n1.start + n1.duration: break
-                n2_can= n2.get_canonical_note()
-
-                self.matching_notes[n1_can][n2_can]+=1
-
-                if j > i: 
-                    self.matching_notes[n2_can][n1_can]+=1
 
     def create_model(self):
         initial_probability= dict( ((s,1.0/len(self.hidden_states)) for s in self.hidden_states) )
@@ -160,13 +142,6 @@ class NarmourHMM(HmmAlgorithm):
                 hmm.remove_state(state)
         hmm.make_walkable()
         self.model= hmm
-        for n, d in self.matching_notes.iteritems():
-            s= sum(d.itervalues())
-            for n2, cnt in d.iteritems():
-                d[n2]=float(cnt)/s
-            #d= dict(sorted(d.iteritems(), key=lambda x:x[1], reverse=True)[:5])
-            #self.matching_notes[n]= set(d.keys())
-        self.matching_notes= dict(self.matching_notes)
         return hmm
 
     def start_creation(self):
@@ -184,55 +159,16 @@ class NarmourHMM(HmmAlgorithm):
         return self.ec.robs
 
 
-    def candidate_pitches(self, now_notes):
-        now_pitches= list(set([n.get_canonical_note() for n in now_notes]))
-        if now_pitches[0] not in self.matching_notes: import ipdb;ipdb.set_trace() 
-
-        res= self.matching_notes[now_pitches[0]].items()
-        res.sort()
-        for pitch in now_pitches[1:]:
-            new_distr= self.matching_notes[pitch].items()
-            new_distr.sort()
-            res= convex_combination(res, new_distr)
-        return res            
-
-    def next_candidates(self, now_notes, min_pitch, max_pitch):
-        if len(now_notes) == 0: return {}
-
-        now_pitches= list(set([n.get_canonical_note() for n in now_notes]))
-        candidate_pitches_distr= self.matching_notes[now_pitches[0]].items()
-        candidate_pitches_distr.sort()
-
-        if abs(sum(i[1] for i in candidate_pitches_distr) -1) > 0.0001:import ipdb;ipdb.set_trace()
-        if len(candidate_pitches_distr) == 0: import ipdb;ipdb.set_trace()            
-        assert len(candidate_pitches_distr) == len(dict(candidate_pitches_distr))
-        candidate_pitches_distr= dict(candidate_pitches_distr)
-
+    def next_candidates(self, candidate_notes_distr, min_pitch, max_pitch):
         last_pitch= self.ec.last_pitch
         last_note= self.ec.last_note
         if last_pitch is None:
-            assert min_pitch % 12  == 0
-            assert max_pitch % 12  == 0
-
-            min_octave= min_pitch/12
-            max_octave= max_pitch/12
-
-            notes_distr= {}
-            for pitch, prob in candidate_pitches_distr.iteritems():
-                for octave in xrange(min_octave, max_octave+1):
-                    notes_distr[pitch.pitch+octave*12]= prob
-            return notes_distr
+            return candidate_notes_distr
         else:
-            candidate_pitches_distr= dict(sorted(candidate_pitches_distr.items(), key=lambda x:x[1], reverse=True)[:5])
-            candidate_pitches_distr=[p.pitch for p in candidate_pitches_distr]
-            candidate_intervals= [Interval(last_note, Note(p)) for p in range(min_pitch, max_pitch+1) if p%12 in candidate_pitches_distr]
-            candidate_intervals= [i for i in candidate_intervals if i.length <= 12]                                      
-            #candidate_intervals= [Interval(last_pitch, p) for p in candidate_pitches_distr]
-            #candidate_intervals.extend((Interval(p, last_pitch)) for p in candidate_pitches_distr)
-            #candidate_intervals= [i for i in candidate_intervals if abs(self.ec.octave - (last_note.pitch + i.length)/12) <= 1]
+            candidate_intervals= [(Interval(last_note, n), n) for n in candidate_notes_distr]
+            candidate_intervals= dict((i, n) for (i, n) in candidate_intervals if i.length <= 12)
 
             available_states= [NarmourInterval(i) for i in candidate_intervals]
-            #import ipdb;ipdb.set_trace()
 
             robs= self.get_current_robs()
             # XXX hacer que la robs ande bien con esto
@@ -244,18 +180,19 @@ class NarmourHMM(HmmAlgorithm):
                 except: import ipdb;ipdb.set_trace()
 
             available_intervals=[ni.interval for ni in available_states if ni == robs.actual_state]
-            available_notes= {}
-            prob= 1.0/len(available_intervals)
-            for interval in available_intervals:
-                available_notes[last_note.pitch + interval.length]= prob
+
+            available_notes= [candidate_intervals[i] for i in available_intervals]
+            available_notes= dict((n, candidate_notes_distr[n]) for n in available_notes)
+            s= sum(available_notes.itervalues())
+            available_notes= dict((n, p/s) for (n,p) in available_notes.iteritems())
 
             return available_notes
 
 
-    @needs('now_chord', 'min_pitch', 'max_pitch')
+    @needs('now_distr', 'min_pitch', 'max_pitch')
     @produces('pitch')
     def next(self, input, result, prev_notes):
-        available_notes= self.next_candidates(input.now_chord.notes, input.min_pitch, input.max_pitch)
+        available_notes= self.next_candidates(input.notes_distr, input.min_pitch, input.max_pitch)
         if len(available_notes) == 0: 
             result.pitch= -1
             return
