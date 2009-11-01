57fcff09987a053bf9b7dca1b9811b50e90edbd9
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-11-01 15:21:20 +0000

    de a una nota

--- src/trunk/electrozart/electrozart/algorithms/hmm/obs_seq_builders.py
+++ src/trunk/electrozart/electrozart/algorithms/hmm/obs_seq_builders.py
@@ -2,6 +2,9 @@ from utils.hmm.random_variable import ConstantRandomVariable
 from electrozart import Score
 
 class ConditionalMidiObsSeq(object):
+    def __init__(self, note_as_hidden_state=False):
+        self.note_as_hidden_state= note_as_hidden_state
+
     def __call__(self, score):
         for instrument in score.instruments:
             if self.condition(instrument):
@@ -23,7 +26,11 @@ class ConditionalMidiObsSeq(object):
             vol_var= ConstantRandomVariable(volume, 'volume')
             start_var= ConstantRandomVariable(note.start, 'start')
             duration_var= ConstantRandomVariable(note.duration, 'duration')
-            res.append((note.duration, {pitch_var : pitch,
+            if self.note_as_hidden_state:
+                state= note
+            else:
+                state= note.duration
+            res.append((state, {pitch_var : pitch,
                                         vol_var : volume,
                                         start_var : note.start,
                                         duration_var: note.duration})) 
@@ -33,9 +40,17 @@ class ConditionalMidiObsSeq(object):
     def condition(self, instrument):
         raise NotImplementedException
 
+class InstrumentObsSeq(ConditionalMidiObsSeq):
+    def __init__(self, instrument, *args, **kwargs):
+        super(InstrumentObsSeq, self).__init__(*args, **kwargs)
+        self.instrument= instrument
+
+    def condition(self, instrument):
+        return self.instrument.patch == instrument.patch and self.instrument.channel == instrument.channel
+
 class FirstVoiceObsSeq(ConditionalMidiObsSeq):
-    def __init__(self, relative=False):
-        super(FirstVoiceObsSeq, self).__init__(self)
+    def __init__(self, relative=False, *args, **kwargs):
+        super(FirstVoiceObsSeq, self).__init__(*args, **kwargs)
         self.relative= relative
 
     def __call__(self, score):
@@ -47,11 +62,11 @@ class FirstVoiceObsSeq(ConditionalMidiObsSeq):
         return self._build_obs_seq(notes)
 
 class MidiPatchObsSeq(ConditionalMidiObsSeq):
-    def __init__(self, patch):
+    def __init__(self, patch, *args, **kwargs):
         """
         patch es el instrumento que me va a interesart seguir
         """
-        ConditionalMidiObsSeq.__init__(self)
+        super(ConditionalMidiObsSeq, self).__init__(*args, **kwargs)
         self.patch= patch
     
     def condition(self, instrument):
@@ -59,11 +74,11 @@ class MidiPatchObsSeq(ConditionalMidiObsSeq):
 
 
 class MidiChannelObsSeq(ConditionalMidiObsSeq):
-    def __init__(self, channel):
+    def __init__(self, channel, *args, **kwargs):
         """
         channel es el canal que importa
         """
-        ConditionalMidiObsSeq.__init__(self)
+        super(MidiChannelObsSeq, self).__init__(*args, **kwargs)
         self.channel= channel
     
     def condition(self, instrument):
