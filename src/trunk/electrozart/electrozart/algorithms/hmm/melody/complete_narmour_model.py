71e3a6a58434fb263c2181ae7f5d8455ef96f311
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-11-07 14:32:56 +0000

    posibilidad de smoothing, nuevo must builder

--- src/trunk/electrozart/electrozart/algorithms/hmm/melody/complete_narmour_model.py
+++ src/trunk/electrozart/electrozart/algorithms/hmm/melody/complete_narmour_model.py
@@ -10,11 +10,21 @@ from electrozart import Note
 class ContourAlgorithm(ListAlgorithm):
     def __new__(cls, *args, **kwargs):
         instance= super(ContourAlgorithm, cls).__new__(cls, *args, **kwargs)
-        instance.params.update(dict(support_note_strategy  = 'random_w_cache'))
+        instance.params.update(dict(support_note_strategy          = 'random_w_cache',
+                                    support_note_tonality_strategy = 'uniform',
+                                    support_note_narmour_strategy  = 'uniform',
+                                    middle_note_tonality_strategy  = 'uniform',
+                                    middle_note_narmour_strategy   = 'uniform'))
         return instance
 
     def __init__(self, *args, **kwargs):
-        self.narmour_features_cnt= defaultdict(lambda : defaultdict(int))
+        super(ContourAlgorithm, self).__init__(*args, **kwargs)
+
+        self.narmour_features_cnt= defaultdict(dict)
+        for feature_name, all_vals in all_features_values().iteritems():
+            for val in all_vals:
+                self.narmour_features_cnt[feature_name][val]=0.5
+
 
     def pick_support_note(self, chord, available_pitches):
         chord_pitches= [n.pitch%12 for n in chord.notes]
@@ -60,7 +70,7 @@ class ContourAlgorithm(ListAlgorithm):
                 if n1.is_silence or n2.is_silence or n3.is_silence: continue
                 features= get_features(n1, n2, n3)                
                 for feature_name, value in features.iteritems():
-                    self.narmour_features_cnt[feature_name][value]+=1
+                    self.narmour_features_cnt[feature_name][value]=1
 
     def start_creation(self):
         super(ContourAlgorithm, self).start_creation()
@@ -76,19 +86,42 @@ class ContourAlgorithm(ListAlgorithm):
         self.ec.last_support_note= None
         self.ec.support_note_cache= {}
 
-    def percent_values(self):
-        res= 1.0/12
-        for feature, vals in self.ec.narmour_features_prob.iteritems():
-            res*= 1.0/len(vals)
-        return res            
+    def percent_value(self, pitches_distr, tonality_strategy, narmour_strategy):
+        if tonality_strategy == 'min':
+            res= min(pitches_distr, key=lambda x:x[1])[1]
+        elif tonality_strategy == 'max':
+            res= max(pitches_distr, key=lambda x:x[1])[1]*0.8
+        elif tonality_strategy == 'uniform':
+            res= 1.0/len(pitches_distr)
+        elif tonality_strategy == 'cero':
+            res= 0
+        else:
+            raise
 
-    @needs('rythm_phrase_len', 'notes_distr', 'now_chord', 'prox_chord', 'min_pitch', 'max_pitch')
+        for feature, vals in self.ec.narmour_features_prob.iteritems():
+            if narmour_strategy == 'min':
+                res*= min(vals.itervalues())
+            elif narmour_strategy == 'max':
+                res*= max(vals.itervalues())*0.8
+            elif narmour_strategy == 'uniform':
+                res*= 1.0/len(vals)
+            elif narmour_strategy == 'cero':
+                res= 0
+            else:
+                raise
+        return res                
+
+    @needs('rythm_phrase_len', 'notes_distr', 'prox_notes_distr', 'pitches_distr', 'prox_pitches_distr', 'now_chord', 'prox_chord', 'min_pitch', 'max_pitch')
     def generate_list(self, input, result, prev_notes):
-        prob_model= ProbModel(self.ec.narmour_features_prob, input.notes_distr)
-        x1= x2= self.percent_values()
+        now_prob_model= ProbModel(self.ec.narmour_features_prob, input.notes_distr)
+        prox_prob_model= ProbModel(self.ec.narmour_features_prob, input.prox_notes_distr)
+
+        support_note_prob= 0#.5#self.percent_value(input.pitches_distr, self.params['support_note_tonality_strategy'], self.params['support_note_narmour_strategy']) 
+        middle_note_prob= 0#.5 #self.percent_value(input.pitches_distr, self.params['middle_note_tonality_strategy'], self.params['middle_note_narmour_strategy']) 
+
         end_pitch= self.pick_support_note(input.prox_chord, set(range(input.min_pitch, input.max_pitch+1)))
 
-        must= build_must_dict(prob_model, end_pitch, input.min_pitch, input.max_pitch, x1, x2, input.rythm_phrase_len)
+        must= build_must_dict(now_prob_model, prox_prob_model, end_pitch, input.min_pitch, input.max_pitch, support_note_prob, middle_note_prob, input.rythm_phrase_len)
 
         if len(prev_notes) == 0: #self.ec.last_support_note is None:
             max_key=max(must.iterkeys())
@@ -97,8 +130,8 @@ class ContourAlgorithm(ListAlgorithm):
             else:
                 # no pude empezar
                 import ipdb;ipdb.set_trace()
+                raise Exception('no pude empezar')
             context= (n1, n2)
-            #start_pitch= self.pick_support_note(input.now_chord, input.min_pitch, input.max_pitch)
         elif len(prev_notes) == 1:
             max_key=max(must.iterkeys())
             for (n1, n2), candidates in must[max_key].iteritems():
@@ -108,6 +141,7 @@ class ContourAlgorithm(ListAlgorithm):
             else:
                 # no pude empezar
                 import ipdb;ipdb.set_trace()
+                raise Exception('no pude empezar')
             context= (n1, n2)
 
         else:
@@ -116,13 +150,23 @@ class ContourAlgorithm(ListAlgorithm):
             context= (context[0].pitch, context[1].pitch)
         
         pitches= []
-        for i in xrange(input.rythm_phrase_len, 0, -1):
-            candidates= must[i][context]
-            d= dict((c, prob_model.get_prob(context[0], context[1], c)) for c in candidates)
-            pitch= RandomPicker(values=d).get_value(normalize=True)
+        if input.rythm_phrase_len == 1:
+            if self.ec.last_support_note is None:
+                pitch= self.pick_support_note(input.now_chord, set(range(input.min_pitch, input.max_pitch+1)))
+            else:
+                pitch= self.ec.last_support_note
             pitches.append(pitch)
+        else:
+            for i in xrange(input.rythm_phrase_len, 0, -1):
+                candidates= must[i][context]
+                if len(candidates)==0: 
+                    import ipdb;ipdb.set_trace()
+                    raise Exception('no candidates!')
+                d= dict((c, now_prob_model.get_prob(context[0], context[1], c)) for c in candidates)
+                pitch= RandomPicker(values=d).get_value(normalize=True)
+                pitches.append(pitch)
 
-            context= (context[-1], pitch)
+                context= (context[-1], pitch)
 
         self.ec.last_support_note= end_pitch
         res= []
@@ -138,21 +182,68 @@ class ContourAlgorithm(ListAlgorithm):
         
 
 
-def build_must_dict(prob_model, nf, min_pitch, max_pitch, x1, x2, length):
+def build_max_must_dict(now_prob_model, nf, min_pitch, max_pitch, support_note_prob, middle_note_prob, length):
     must= defaultdict(lambda : defaultdict(set)) 
     for n1 in xrange(min_pitch, max_pitch):
         for n2 in xrange(min_pitch, max_pitch):
-            for n3 in xrange(min_pitch, max_pitch):
-                if prob_model.get_prob(n2, n3, nf, harmony=False) >= x1:
-                    must[1][(n1, n2)].add(n3)
+            must[1][(n1, n2)]= set([max([n3 for n3 in xrange(min_pitch, max_pitch)], key= lambda n: now_prob_model.get_prob(n2, n, nf, harmony=False))])
 
     for i in xrange(2, length+1):
         for n1 in xrange(min_pitch, max_pitch+1):
             for n2, n3 in must[i-1]:
-                if prob_model.get_prob(n1, n2, n3) >= x2:
+                must[i][(n1, n2)]= set([max([n3 for n3 in xrange(min_pitch, max_pitch)], key= lambda n: now_prob_model.get_prob(n2, n, nf, harmony=False))])
+
+    return must
+
+def build_median_must_dict(now_prob_model, prox_prob_model, nf, min_pitch, max_pitch, support_note_prob, middle_note_prob, length):
+    must= defaultdict(lambda : defaultdict(list)) 
+    def apply_percent(i, p):
+        for context in must[i]:
+            l= must[i][context]
+            l.sort(key=lambda x:x[1], reverse=True)
+            size= int(p*len(l) + 1)
+            must[i][context]= set(n[0] for i, n in enumerate(l) if i < size)
+
+    for n1 in xrange(min_pitch, max_pitch+1):
+        for n2 in xrange(min_pitch, max_pitch+1):
+            for n3 in xrange(min_pitch, max_pitch+1):
+                must[1][(n1, n2)].append((n3, prox_prob_model.get_prob(n2, n3, nf)))
+
+    apply_percent(1, support_note_prob)            
+
+    for i in xrange(2, length+1):
+        for n1 in xrange(min_pitch, max_pitch+1):
+            for n2, n3 in must[i-1]:
+                n3prob= now_prob_model.get_prob(n1, n2, n3)
+                must[i][(n1, n2)].append((n3, n3prob))
+
+        apply_percent(i, middle_note_prob)            
+
+
+
+    return must
+
+def build_percent_must_dict(now_prob_model, prox_prob_model, nf, min_pitch, max_pitch, support_note_prob, middle_note_prob, length):
+    import ipdb;ipdb.set_trace()
+    must= defaultdict(lambda : defaultdict(set)) 
+    for n1 in xrange(min_pitch, max_pitch+1):
+        for n2 in xrange(min_pitch, max_pitch+1):
+            for n3 in xrange(min_pitch, max_pitch+1):
+                if prox_prob_model.get_prob(n2, n3, nf) >= support_note_prob:
+                    must[1][(n1, n2)].add(n3)
+
+    m= None
+    for i in xrange(2, length+1):
+        for n1 in xrange(min_pitch, max_pitch+1):
+            for n2, n3 in must[i-1]:
+                n3prob= now_prob_model.get_prob(n1, n2, n3)
+                m= max(m, n3prob)
+                if n3prob >= middle_note_prob:
                     must[i][(n1, n2)].add(n3)
 
     return must
+build_must_dict= build_median_must_dict    
+
 class ProbModel(object):
     def __init__(self, narmour_features_prob, notes_distr):
         self.narmour_features_prob= narmour_features_prob
@@ -165,6 +256,7 @@ class ProbModel(object):
         if harmony: res= self.notes_distr[n3]
         else: res= 1.0
         for k, v in features.iteritems():
+            #if v not in self.narmour_features_prob[k]: import ipdb;ipdb.set_trace()
             res*= self.narmour_features_prob[k][v]
         return res            
 
@@ -225,6 +317,25 @@ def get_features(n1, n2, n3):
     else:
         features['rr']= 0
 
+    #features['id pr cl']= features['id'], features['pr'], features['cl']
+    #features.pop('id')
+    #features.pop('pr')
+    #features.pop('cl')
+
     return features
 
 
+def all_features_values():
+    res=defaultdict(list)
+
+    #for id_val in (0, 1):
+    #    for cl_val in (0, 1, 2):
+    #        for pr_val in (0, 1, 2):
+    #            res['id pr cl'].append((id_val, pr_val, cl_val))
+    
+    res['id']= [0,1]
+    res['cl']= [0,1,2]
+    res['pr']= [0,1,2]
+    res['rd']= [0,1,2]
+    res['rr']= [0,1]
+    return res
