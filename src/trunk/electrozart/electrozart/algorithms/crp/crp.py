017aa5d75a1c1b41f40c5b4565d14e0412e8cd02
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-11-09 18:27:32 +0000

    random

--- src/trunk/electrozart/electrozart/algorithms/crp/crp.py
+++ src/trunk/electrozart/electrozart/algorithms/crp/crp.py
@@ -1,10 +1,12 @@
 from utils.hmm.random_variable import RandomPicker
 
 class ChineseRestaurantProcess(object):
-    def __init__(self, alpha):
+    def __init__(self, alpha, random=None):
         self.alpha= alpha
         self.customers_per_table= {}
         self.ntables= 0
+        if random is None: import ipdb;ipdb.set_trace()
+        self.random=random
 
     def next(self):
         if self.ntables == 0:
@@ -15,7 +17,7 @@ class ChineseRestaurantProcess(object):
             s= float(sum(self.customers_per_table.itervalues()))
             v= dict((t, n/(s+self.alpha)) for (t,n) in self.customers_per_table.iteritems())
             v[-1]= self.alpha/(s+self.alpha)
-            prox_table= RandomPicker(values=v).get_value()
+            prox_table= RandomPicker(values=v, random=self.random).get_value()
             if prox_table == -1:
                 prox_table= self.ntables 
                 self.customers_per_table[self.ntables]= 1
