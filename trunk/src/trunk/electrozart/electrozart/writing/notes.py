4834349dc391af0ba39447db7ddc78bf513453d7
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2009-12-03 12:18:37 +0000

    writer mejor escrito

--- trunk/src/trunk/electrozart/electrozart/writing/notes.py
+++ trunk/src/trunk/electrozart/electrozart/writing/notes.py
@@ -3,9 +3,10 @@ from base import ScoreWriter
 class NotesScoreWriter(ScoreWriter):
     def dump(self, score, fname):
         notes= score.get_first_voice()
+        notes= score.get_notes(skip_silences=True)
         f=open(fname, 'w')
         for note in notes:
-            if note.is_silence: continue
+            #if note.is_silence: continue
             d= dict(start = note.start,
                     finish= note.start + note.duration,
                     pitch= note.pitch)
