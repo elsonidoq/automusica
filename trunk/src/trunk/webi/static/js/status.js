3452316e33c319c1f903243101479491dafd15b3
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2010-01-21 02:11:30 +0000

    status separado

---
+++ trunk/src/trunk/webi/static/js/status.js
@@ -0,0 +1,53 @@
+var Status= function(elem) {
+    this.elem= elem;
+    this.queue= new Array();
+    this.doing_something= false;
+    this.can_clear_queue= true;
+}
+Status.prototype.show_status= function(text) {
+    //console.log('show_status ' + this.doing_something);
+    if (this.doing_something) {
+        this.queue.push(text);
+        return;
+    }
+    this.doing_something= true;
+    if (this.elem.is(":visible")) {
+        var the_elem= this.elem;
+        var the_this= this;
+        this.elem.fadeOut(100, function() {
+            the_elem.text(text);
+            the_elem.fadeIn(100);
+            the_this.set_doing();
+        });
+    } else {
+        this.elem.text(text);
+        var the_this= this;
+        this.elem.slideDown(callback=function() {the_this.set_doing()});
+    }
+}
+
+Status.prototype.hide_status= function() {
+    //console.log('hide_status ' + this.doing_something);
+    if (this.doing_something) {
+        this.queue.push(null);
+        return;
+    }
+    this.doing_something= true;
+    the_this= this;
+    this.elem.slideUp(callback=function() {the_this.set_doing()});
+}
+
+
+Status.prototype.set_doing= function() {
+    this.doing_something= (this.queue.length > 0);
+    if (this.queue.length > 0) {
+        var msg= this.queue.pop();
+        //console.log('Processing ' + msg);
+        if (msg) {
+            this.show_status(msg);
+        } else {
+            this.hide_status();
+        }
+    }
+}
+
