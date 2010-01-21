cdd00782ba2c60b49ba7f88ce2df9a60fc6df1cc
elsonidoq <elsonidoq@45c3a679-0544-0410-ace8-df16a8615bab>
2010-01-21 03:37:39 +0000

    por fin sound

--- trunk/src/trunk/webi/static/js/status.js
+++ trunk/src/trunk/webi/static/js/status.js
@@ -2,31 +2,27 @@ var Status= function(elem) {
     this.elem= elem;
     this.queue= new Array();
     this.doing_something= false;
-    this.can_clear_queue= true;
 }
-Status.prototype.show_status= function(text) {
-    console.log('show_status ' + text);
+Status.prototype.show_status= function(text, on_queue) {
     //console.log('show_status ' + this.doing_something);
-    if (this.doing_something) {
+    if (this.doing_something && !on_queue) {
         this.queue.push(text);
         return;
     }
     this.doing_something= true;
     this.elem.text(text);
+
     var the_this= this;
-    console.log("Sliding");
     this.elem.slideDown(500,callback=function() {the_this.set_doing()});
-    console.log("Ya Slide");
 }
 
-Status.prototype.hide_status= function() {
-    console.log('hide_status ');
-    //console.log('hide_status ' + this.doing_something);
-    if (this.doing_something) {
+Status.prototype.hide_status= function(on_queue) {
+    if (this.doing_something && !on_queue) {
         this.queue.push(null);
         return;
     }
     this.doing_something= true;
+
     the_this= this;
     this.elem.slideUp(callback=function() {the_this.set_doing()});
 }
@@ -35,12 +31,11 @@ Status.prototype.hide_status= function() {
 Status.prototype.set_doing= function() {
     this.doing_something= (this.queue.length > 0);
     if (this.queue.length > 0) {
-        var text= this.queue.pop();
-        console.log('Queue ' + text);
+        var text= this.queue.splice(0,1)[0];
         if (text) {
-            this.show_status(text);
+            this.show_status(text, on_queue=true);
         } else {
-            this.hide_status();
+            this.hide_status(on_queue=true);
         }
     }
 }
